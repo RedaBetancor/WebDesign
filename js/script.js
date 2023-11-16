56b836393abb205154f1e26446e034e5541b5cac
RedaBetancor <mohamedredazinatharrounbetancor@alumno.ieselrincon.es>
2023-11-16 12:02:10 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.getElementById("burguer").addEventListener("click", () => {
+	var cross = document.getElementById("cross");
+	cross.classList.remove("hidden");
+	var burguer = document.getElementById("burguer");
+	burguer.classList.add("hidden");
+})
+
+document.getElementById("cross").addEventListener("click", () => {
+	var cross = document.getElementById("cross");
+	cross.classList.add("hidden");
+	var burguer = document.getElementById("burguer");
+	burguer.classList.remove("hidden");
+})
+
+function initialize() {
+	window.addEventListener("scroll", moveImagesWhenUserScrolls);
+}
+
+initialize();
