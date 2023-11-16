c805d30cbc4e853a55c9c9af960d68cde7824daf
RedaBetancor <mohamedredazinatharrounbetancor@alumno.ieselrincon.es>
2023-11-16 23:55:08 +0000

    third commit

---
+++ js/store.js
@@ -0,0 +1,56 @@
+document.getElementById("button").addEventListener("click",  (e)=> {
+    e.preventDefault();
+  
+    const productName = document.getElementById("productName").value;
+    const quantity = document.getElementById("quantity").value;
+    const taste = document.getElementById("taste").value;
+    const brand = document.getElementById("brand").value;
+  
+    if (productName.trim() === '' || quantity.trim() === '' || taste.trim() === ''|| brand.trim() === '' ) {
+      alert("Por favor, completa todos los campos del formulario.");
+      return;
+    }
+  
+    const productData = {
+      name: productName,
+      taste: taste,
+      brand: brand,
+      quantity: quantity
+    };
+  
+    agregarProductoALista(productData);
+    document.getElementById("productForm").reset();
+});
+
+function agregarProductoALista(producto) {
+    const productListContainer = document.getElementById("productList");
+  
+    const productCard = document.createElement("div");
+    productCard.className = "card m-2";
+    productCard.style = "width: 18rem;";
+  
+    productCard.innerHTML = `
+      <div class="card-body">
+        <h5 class="card-title">${producto.name}</h5>
+        <p class="card-text">Marca: ${producto.brand}</p>
+        <p class="card-text">Sabor: ${producto.taste}</p>
+        <p class="card-text">Cantidad: ${producto.quantity}</p>
+      </div>
+    `;
+  
+    productListContainer.appendChild(productCard);
+}
+
+document.getElementById("menu").addEventListener("click", ()=>{
+    var cross = document.getElementById("cross");
+    cross.classList.remove("hidden");
+    var menu = document.getElementById("menu");
+    menu.classList.add("hidden");
+})
+
+document.getElementById("cross").addEventListener("click", () => {
+  var cross = document.getElementById("cross");
+  cross.classList.add("hidden");
+  var menu = document.getElementById("menu");
+  menu.classList.remove("hidden");
+})
